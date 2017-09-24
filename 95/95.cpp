27eace395fdd5f904b69ae7717bcf900ad7f4f0d
relive <relive@g.com>
2017-09-24 11:41:46 +0800

    modify

--- 95/95.cpp
+++ 95/95.cpp
@@ -1,50 +1,63 @@
-#include <iostream>
-#include <string>
-#include <map>
-#include <vector>
-#include <limits.h>
-#include <queue>
-#include <stack>
-#include <sstream>
-#include <algorithm>
-#include <set>
+#include "../helpers.cpp"
 
-using namespace std;
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
+vector<int> preOrder = {100,50, 40, 80, 60, 200, 150, 180, 250};
 class Solution {
-	public:
-	vector<TreeNode*> generateTrees(int n){
-		
-	}
-
-	TreeNode* buildBST(int root, int ll, int rr)
-	{
-		TreeNode* node = new TreeNode(root);
-		if(ll==rr)
-			return new TreeNode(ll);
+        public:
+        vector<TreeNode*> generateTrees(int n){
+		return helper(1, n);
+        }
+	vector<TreeNode*> helper(int ll, int rr){
+		vector<TreeNode*> res;
+		TreeNode* tmp;
+		if (ll==rr){
+			tmp = new TreeNode(ll);
+			res.push_back(tmp);
+			return res;
+		}
+		if (ll > rr)
+			return res;
+		for (int i = ll ; i <= rr ; i++){
+			vector<TreeNode*> left = helper(ll, i-1);
+			vector<TreeNode*> right = helper(i+1, rr);
+			if (left.empty()){
+				for (int m = 0 ; m < right.size() ; m++){
+					tmp = new TreeNode(i);
+					tmp->right = right[m];
+					res.push_back(tmp);
+				}
+			} else if (right.empty()){
+				for (int m = 0 ; m < left.size() ; m++){
+					tmp = new TreeNode(i);
+					tmp->left = left[m];
+					res.push_back(tmp);
+				}
+			} else {
+				for (int m = 0 ; m < left.size() ; m++){
+					for (int n = 0 ; n < right.size() ;n++)	{
+						tmp = new TreeNode(i);
+						tmp->left = left[m];
+						tmp->right = right[n];
+						res.push_back(tmp);
+					}
+				}
+			}
+		}
+		return res;
 	}
 };
-
 #define genVector(data, array) vector<int> data(array, array+sizeof(array)/sizeof(array[0]));
 int main()
 {
-	int A[] = {1,2,3};
-	int B[] = {1,2,3};
-	string sa("godding");
-	string sb("gd");
-	genVector(da, A);
-	genVector(db, B);
+	vector<int> A = {1,2,3};
+	vector<int> B = {1,2,3};
+	string tre("1,#,3,2,#,#,4,#,5,#,#");
+	Codec cc;
+	//TreeNode* node = cc.deserialize(tre);
+	//cout <<  cc.serialize(node) << endl;
 	Solution sol;
-	vector<int> ret_vec;
 	int ret;
 	ret = sol.
-	ret_vec = sol.
+	auto ret_vec = sol.
 	cout << ret << "\n";
 
 	for (int i = 0 ; i < ret_vec.size() ; i++)
--- _lock/484/484.cpp
+++ _lock/484/484.cpp
@@ -5,36 +5,54 @@ class Solution {
         public:
         vector<int> findPerm(string sign){
 		vector<int> res;
-		int cur = 1, premax = 1;
+		int cur = 1;
 		int dcnt = 0;
 		for (int i = 0 ; i < sign.size() ; i++){
 			if (sign[i]=='D'){
 				dcnt++;
-				premax++;
+				cur++;
 			} else if (sign[i]=='I'){
 				for (int j = 0 ; j < dcnt ; j++){
-					res.push_back(premax--);
+					res.push_back(cur--);
 				}
 				res.push_back(cur);
 				cur = cur + dcnt + 1;
-				premax = cur;
 				dcnt = 0;
 			}
 		}
 		for (int j = 0 ; j < dcnt ; j++)
-			res.push_back(premax--);
+			res.push_back(cur--);
 		res.push_back(cur);
 		return res;
         }
+#if 0
+	vector<int> findPerm2(string sign){
+		vector<int> res;
+		int cur = 1;
+		for (int i = 0 ; i < sign.size() ; i++){
+			if (sign[i]=='D'){
+				int j = i;
+				int cnt = 1;
+				while(j <sign.size() && sign[j]=='D'){
+					j++;
+					cnt++;
+				}
+				if (res.empty())
+					res.push_back(cur+cnt);
+				for (k = 0; k <)	
+			}
+		}
+	}
+#endif
 };
 #define genVector(data, array) vector<int> data(array, array+sizeof(array)/sizeof(array[0]));
 int main()
 {
 	Solution sol;
-	//auto ret_vec = sol.findPerm("DDDIDD");
+	auto ret_vec = sol.findPerm("DDIIDD");
 	//auto ret_vec = sol.findPerm("IDDDI");
 	//auto ret_vec = sol.findPerm("IDDIDDDI");
-	auto ret_vec = sol.findPerm("IDIIDD");
+	//auto ret_vec = sol.findPerm("DIII");
 
 	for (int i = 0 ; i < ret_vec.size() ; i++)
 		cout << ret_vec[i] << " ";
